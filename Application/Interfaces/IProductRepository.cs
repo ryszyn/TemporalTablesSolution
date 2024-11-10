cd69a1df461f83602d26773ecc070242522eac82
Ryszard Witkowski <ryszard.witkowski@navigal.eu>
2024-11-10 17:08:21 +0100

    second commit

---
+++ Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Interfaces;
+
+using Domain.Entities;
+
+public interface IProductRepository
+{
+    Task AddAsync(Product product);
+    Task DeleteAsync(int id);
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task<Product> GetByIdAsync(int id);
+    Task<IEnumerable<Product>> GetHistoryAsync(int id);
+    Task UpdateAsync(Product product);
+}
---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public DateTime ValidFrom { get; set; }
+    public DateTime ValidTo { get; set; }
+}
